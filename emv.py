788fa0023eec0c2e8033fadaeed08c847a9bc898
cartman1978 <cartman_7802@yahoo.ie>
2021-06-03 20:45:54 +0000

    add register functionality

--- emv.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ.setdefault("IP", "0.0.0.0")
-os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("SECRET_KEY", "Ufwe,~OQn75r!|9NPp#L~hW2)YQ:i/")
-os.environ.setdefault("MONGO_URI", "mongodb+srv://root:Manolino18@firstcluster.fq0o7.mongodb.net/task_manager?retryWrites=true&w=majority")
-os.environ.setdefault("MONGO_DBNAME", "task_manager")
